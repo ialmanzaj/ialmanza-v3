717fd19c371ab78406d80ce072c60afa104a6c72
Isaac Almanza <isaac.almanza@apoyomultiple.com>
2023-06-03 12:23:04 -0500

    fix: enable effect dark-light with css and change toggle button

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -11,24 +11,28 @@ export default function ThemeToggle() {
   const iconCN = 'z-10 h-8 w-8 p-[0.4rem]';
 
   return (
-    <div className="relative flex h-10 w-[5.5rem] cursor-pointer items-center justify-between rounded-full bg-gradient-to-br from-gray-50 to-white px-2 shadow-md shadow-gray-400 transition-all duration-500 dark:from-gray-700 dark:to-customGray-dark dark:shadow-black">
-      {mounted && (
-        <>
-          <BsSun
+    <>
+    <button
+     onClick={() =>
+      setTheme(
+        theme === 'light' ? 'dark' : theme === 'system' ? 'dark' : 'light'
+      )
+    }
+    className='p-2 ml-1 hover:bg-gray-200 dark:hover:bg-gray-700 cursor-pointer rounded-lg dark:text-gray-100'
+    >
+    {mounted && theme === 'light' ? (
+             <BsMoonFill
+             className={`${iconCN} text-black`}
+             onClick={() => setTheme('dark')}
+           />
+        ) : (
+      
+            <BsSun
             className={`${iconCN} text-white`}
             onClick={() => setTheme('light')}
           />
-          {theme === 'light' ? (
-            <BsMoon className={iconCN} onClick={() => setTheme('dark')} />
-          ) : (
-            <BsMoonFill
-              className={`${iconCN} text-black`}
-              onClick={() => setTheme('dark')}
-            />
-          )}
-        </>
-      )}
-      <span className="absolute left-[8px] h-8 w-8 rounded-full bg-black transition-all duration-500 dark:left-auto dark:translate-x-[40px] dark:bg-white" />
-    </div>
+        )}
+    </button>
+    </>
   );
 }
