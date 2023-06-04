eef23286dd283b3dfb57565038809b6176921648
Isaac Almanza <isaac.almanza@apoyomultiple.com>
2023-06-04 14:10:37 -0500

    feat: add space font and improve font style and home copy

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,7 @@ import ScrollUpButton from '@/components/scroll-up-button';
 import '@/styles/globals.css';
 import '@/styles/paginate.css';
 import { Analytics } from '@vercel/analytics/react';
+import {Space_Grotesk} from '@next/font/google'
 
 
 export const metadata = {
@@ -16,6 +17,10 @@ export const metadata = {
     template: '%s | Isaac Almanza',
   },
 };
+const space_grotesk = Space_Grotesk({
+  subsets: ['latin'],
+  weight: ['300', '400', '700']
+})
 
 export default function RootLayout({
   children,
@@ -27,7 +32,7 @@ export default function RootLayout({
       <body className="text-primary bg-primary relative mx-auto mb-20 flex w-full max-w-screen-xl flex-col px-[6vw] md:px-[5vw]">
         <Provider>
           <Header />
-          <main>{children}</main>
+          <main className={space_grotesk.className}>{children}</main>
           <div className="fixed bottom-12 right-10">
             <ScrollUpButton />
           </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,11 +8,16 @@ export default function HomePage() {
   return (<>
       <section>
       <GlitchApp />
-      <div className="max-w-xl mx-auto mt-20 flex-1">
-        <h1 className="font-bold text-3xl font-serif">Hi, I&apos;m Isaac Almanza</h1>
-        <p className="my-5">
-        Isaac is a self-taught, independent builder, educator, and software engineer who focuses on cutting-edge fintech and web3 solutions. 
-        He is currently building a neobank based out of Mexico. Isaac likes to occupy himself learning and buidling in web3 with complex tasks to push the limits of what current exists in the market.
+      <div className="max-w-2xl mx-auto mt-20 flex-1">
+        <div className="my-12">
+          <h1 className="tracking-wide text-6xl">
+            <span className="leading-10">Welcome 👋🏽<br></br></span>
+             <span className="leading-10">
+             to the Home of Isaac</span></h1>
+        </div>
+        <p className=" font-light text-lg	leading-9	tracking-normal	">
+        Isaac Almanza is a software engineer, builder, educator who focuses on building cutting-edge fintech and web3 products. 
+        He is currently building a neobank based out of Mexico. He likes to occupy himself learning and building in web3 with complex tasks to push the limits of what current exists in the market.
         </p>
       </div>
       </section>
