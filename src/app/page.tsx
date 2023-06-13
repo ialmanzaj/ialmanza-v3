04622993fc12aa3c5bb3f084cdd3d0a62f2bcf1b
Isaac Almanza <isaac.almanza@apoyomultiple.com>
2023-06-13 09:42:21 -0500

    fix: fix unescaped-entities

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,16 +12,16 @@ export default function HomePage() {
       <div className="max-w-2xl mx-auto mt-20 flex-1">
         <div className="my-12">
           <h1 className="tracking-wide text-6xl">
-            <span className="leading-12">Hello 👋🏽<br></br> I'm Isaac Almanza</span>
+            <span className="leading-12">Hello 👋🏽<br></br> I&apos;m Isaac Almanza</span>
              
           </h1>
         </div>
         <p className=" font-light text-lg	leading-9	tracking-normal">
-        I'm a software engineer and entrepreneur. I have been building fintech products for the last 5 years in Latam. Most recently at Fondeadora, neobank based out of Mexico.  
+        I&apos;m a software engineer, entrepreneur and digital nomad based in Latam. I have been building fintech products for the last 5 years in Latam. Most recently at Fondeadora, neobank based out of Mexico.  
         Now transitioning into web3 focusing on smart contracts with solidity, user interfaces, and everything in between. 
         </p><br></br>
         <p className=" font-light text-lg	leading-9	tracking-normal">
-      He is curious person who loves getting into new rabbit holes, such as philosophy, mental models, entrepreneurship, and other interesting ideas.
+       I am curious person who loves getting into new rabbit holes, such as philosophy, mental models, web3/blockchain, and other interesting ideas.
         </p>
         <Rotator />
       </div>
