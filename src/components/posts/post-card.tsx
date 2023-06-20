ae3c8472b92632691f512457c7f4225d4330db74
Isaac Almanza <isaac.almanza19@gmail.com>
2023-06-20 12:58:58 -0500

    fix: fix blog css

--- src/components/posts/post-card.tsx
+++ src/components/posts/post-card.tsx
@@ -13,7 +13,7 @@ export default function PostCard({
 }) {
   return (
     <Link href={`/blog/${slug}`}>
-      <article className="mx-auto flex max-w-[25rem] flex-col overflow-hidden rounded-xl shadow-xl shadow-gray-400 transition-all duration-300 hover:scale-[1.05] hover:shadow-2xl dark:shadow-black">
+      <article className="mx-auto flex max-w-[25rem] flex-col overflow-hidden rounded-xl shadow-xl transition-all duration-300 hover:scale-[1.05] hover:shadow-2xl dark:shadow-black">
         <div className="relative h-60">
           <Image
             src={cover}
@@ -24,7 +24,7 @@ export default function PostCard({
         </div>
         <div className="flex h-48 flex-col p-4">
           <h3 className="line-clamp-2 h-16 text-2xl font-bold">{title}</h3>
-          <time className="mb-4 mt-2 pl-2 text-sm text-gray-400">{date}</time>
+
           <CategoryList categories={categories} />
         </div>
       </article>
